fc94661395b67f49cc43e36de389d3d8a35f09ce
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2023-09-03 12:45:48 +0300

    Improve randomness by separating vocals from consonants

--- randomness.lisp
+++ randomness.lisp
@@ -3,7 +3,7 @@
 (in-package #:cloned-natural-language)
 
 (defparameter *alphabet*
-  #(#\a #\b #\c #\d #\e #\f #\g #\h #\i
+  '(#\a #\b #\c #\d #\e #\f #\g #\h #\i
     #\j #\k #\l #\m #\n #\o #\p #\q #\r
     #\s #\t #\u #\v #\w #\x #\y #\z #\ă
     #\â #\î #\ț #\ș))
@@ -12,14 +12,13 @@
   "Documentation for rand-elt with parameters lst"
   (nth (random (length lst)) lst))
 
-(defun create-word-from-randomness ()
-  "Documentation for create-word with parameters "
-  (let ((len (+ 2 (random 8)))
-        (vocals (loop for x in '(#\a #\e #\i #\o #\u #\ă #\â #\î)
-                      collect (search (list x) *alphabet*))))
-    (format nil "~{~a~}"
-            (loop for c below len
-                  collect (aref *alphabet*
-                                (if (/= 0 (mod c 2))
-                                    (random (length *alphabet*))
-                                    (rand-elt vocals)))))))
+(let* ((vocals '(#\a #\e #\i #\o #\u #\ă #\â #\î))
+       (consonants (set-difference *alphabet* vocals)))
+  (defun create-word-from-randomness ()
+    "Documentation for create-word with parameters "
+    (let ((len (+ 2 (random 8))))
+      (format nil "~{~a~}"
+              (loop for c below len
+                    collect (if (/= 0 (mod c 2))
+                                (rand-elt consonants)
+                                (rand-elt vocals)))))))
